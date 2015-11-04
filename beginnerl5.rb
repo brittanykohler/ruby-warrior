004776642fc2567f278ba8f2dd90258f3cd2b875
Brittany <BrittanyKohler@gmail.com>
2015-11-03 18:53:30 -0800

    Adding level 5

---
+++ beginnerl5.rb
@@ -0,0 +1,20 @@
+class Player
+  def initialize
+    @health = 20
+  end
+
+  def play_turn(warrior)
+    if warrior.feel.enemy?
+      warrior.attack!
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    elsif warrior.health < @health
+      warrior.walk!
+    elsif warrior.health < 14
+      warrior.rest!
+    else
+      warrior.walk!
+    end
+    @health = warrior.health
+  end
+end
